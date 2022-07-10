6dbb2ce83379689b2e87f412aea4b4ad5b6bd1f7
YuzukiTsuru <740291272@qq.com>
2022-07-10 22:36:31 +0800

    add delay

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 let handsontableContainer = document.getElementById('handsontable-container')
-
 let url = "https://pilipili.space/awchips/data/SUNXI_CHIP.csv"
 
-let data = Papa.parse(url, {
-  download: true,
-  header: true,
-  skipEmptyLines: true,
-  complete: function (results) {
-    // reset container
-    handsontableContainer.innerHTML = ''
-    handsontableContainer.className = ''
 
-    Handsontable(handsontableContainer, {
-      data: results.data,
-      rowHeaders: true,
-      colHeaders: results.meta.fields,
-      columnSorting: true,
-      filters: true,
-      dropdownMenu: true,
-      width: '100%',
-      licenseKey: 'non-commercial-and-evaluation',
-    })
-  }
-})
+setTimeout(function () {
+  Papa.parse(url, {
+    download: true,
+    header: true,
+    skipEmptyLines: true,
+    complete: function (results) {
+      // reset container
+      handsontableContainer.innerHTML = ''
+      handsontableContainer.className = ''
+
+      Handsontable(handsontableContainer, {
+        data: results.data,
+        rowHeaders: true,
+        colHeaders: results.meta.fields,
+        columnSorting: true,
+        filters: true,
+        dropdownMenu: true,
+        width: '100%',
+        licenseKey: 'non-commercial-and-evaluation',
+      })
+    }
+  })
+}, 500)
